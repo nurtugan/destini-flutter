0f35cd86e3aad427b27ce3977561974255ce6f5b
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-11-19 20:23:51 +0600

    Task completed

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -57,5 +57,7 @@ class StoryBrain {
     _storyNumber = 0;
   }
 
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible() {
+    return _storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2;
+  }
 }
