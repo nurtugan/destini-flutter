c904c50582f5df2780af02b2cc60b1b49ca6bf37
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-11-19 20:04:17 +0600

    Helper methods added

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -34,13 +34,12 @@ class StoryBrain {
   ];
 
   String getStory() => _storyData.first.storyTitle;
-}
+
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
-//TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
+  String getChoice1() => _storyData.first.choice1;
 
-//TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
-
+  String getChoice2() => _storyData.first.choice2;
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
 //TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
@@ -54,3 +53,4 @@ class StoryBrain {
 //TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+}
