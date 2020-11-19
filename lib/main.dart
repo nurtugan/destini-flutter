feee2a1ac69db1433437bf3c6ff1f0c68c8eeda0
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-11-19 20:17:07 +0600

    Base logic configured

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,11 @@ class _StoryPageState extends State<StoryPage> {
                 child: FlatButton(
                   onPressed: () {
                     //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    setState(
+                      () {
+                        storyBrain.nextStory(1);
+                      },
+                    );
                   },
                   color: Colors.red,
                   child: Text(
@@ -72,7 +76,11 @@ class _StoryPageState extends State<StoryPage> {
                 child: FlatButton(
                   onPressed: () {
                     //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    setState(
+                      () {
+                        storyBrain.nextStory(2);
+                      },
+                    );
                   },
                   color: Colors.blue,
                   child: Text(
@@ -90,7 +98,5 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
 
 //TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
